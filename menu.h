27f19cb391dbc31eebd22fb33170366476809301
Ksenia Kogan <kseniyakogan29@gmail.com>
2022-03-01 18:09:37 +0100

    Menu

---
+++ menu.h
@@ -0,0 +1,19 @@
+#ifndef MENU_H
+#define MENU_H
+
+using namespace std;
+
+void menu()
+{
+	cout << "\n-----------------------------------------------------------------------" << endl;
+	cout << "\nTasks: \n" << endl;
+	cout << "\n1 - Calculate the total area of apartments, located in the specified" << endl;
+	cout << "\n   district (the name of the district is entered from the keyboard).\n" << endl;
+	cout << "\n2 - Display data about all apartments, located on the last floor" << endl;
+	cout << "\n   of multi-storey building (5 and more floors).\n" << endl;
+	cout << "\nTo finish the program enter: -1" << endl;
+	cout << "\nTo repeat the menu call, enter: 0" << endl;
+	cout << "\n-----------------------------------------------------------------------" << endl;
+}
+
+#endif // MENU_H
